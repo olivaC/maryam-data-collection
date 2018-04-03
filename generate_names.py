141e52efad3c04d2b1b1281baa45ff9ec779ad49
olivaC <coliva@ualberta.ca>
2018-04-03 12:24:22 -0600

    Create generate_names to extract names from the ISSTA website

---
+++ generate_names.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+from bs4 import BeautifulSoup
+
+import urllib.request
+import re
+
+
+def generate_names(url):
+    """
+    This function will write to a .txt file a list of researchers names in the <h3> tag.
+    This is custom made to work for the Program Committee ISSTA 2018 Technical Papers.
+
+    :param url: The URL containing a webpage of researchers
+    :return: None
+    """
+    soup = BeautifulSoup(urllib.request.urlopen(url), "html.parser")
+    headings = soup.find_all(["h3"])
+    file = open("names.txt", "a")
+    for i in headings:
+        i = str(i)
+        if i.startswith('<h3 class="media-heading'):
+            result = re.search('<h3 class="media-heading">(.*) <small>', i)
+            file.write(result.group(1)+"\n")
+
+
+def main():
+    url = input("Enter in URL: ")
+    generate_names(url)
+
+
+if __name__ == "__main__":
+    main()
